325e2c3e341fded1508ca242df0874dcd81faf83
synctech-project <matinmje@gmail.com>
2025-08-21 19:54:09 +0330

    Update passwall.sh

--- passwall.sh
+++ passwall.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
@@ -12,231 +12,119 @@ echo "Running as root..."
 sleep 2
 clear
 
+# System & network config
 uci set system.@system[0].zonename='Asia/Tehran'
-
 uci set network.wan.peerdns="0"
-
 uci set network.wan6.peerdns="0"
-
 uci set network.wan.dns='1.1.1.1'
-
 uci set network.wan6.dns='2001:4860:4860::8888'
-
 uci set system.@system[0].timezone='<+0330>-3:30'
-
 uci commit system
-
 uci commit network
-
 uci commit
-
 /sbin/reload_config
 
+SNNAP=$(grep -o SNAPSHOT /etc/openwrt_release | sed -n '1p')
 
-SNNAP=`grep -o SNAPSHOT /etc/openwrt_release | sed -n '1p'`
-
-if [ "$SNNAP" == "SNAPSHOT" ]; then
-
-echo -e "${YELLOW} SNAPSHOT Version Detected ! ${NC}"
-
-rm -f passwalls.sh && wget https://raw.githubusercontent.com/amirhosseinchoghaei/Passwall/main/passwalls.sh && chmod 777 passwalls.sh && sh passwalls.sh
-
-exit 1
-
- else
-           
-echo -e "${GREEN} Updating Packages ... ${NC}"
-
+if [ "$SNNAP" = "SNAPSHOT" ]; then
+    echo "${YELLOW} SNAPSHOT Version Detected ! ${NC}"
+    rm -f passwalls.sh
+    wget https://raw.githubusercontent.com/amirhosseinchoghaei/Passwall/main/passwalls.sh
+    chmod 777 passwalls.sh
+    sh passwalls.sh
+    exit 1
+else
+    echo "${GREEN} Updating Packages ... ${NC}"
 fi
 
-### Update Packages ###
-
+# Update & add feeds
 opkg update
-
-### Add Src ###
-
 wget -O passwall.pub https://master.dl.sourceforge.net/project/openwrt-passwall-build/passwall.pub
-
 opkg-key add passwall.pub
+: > /etc/opkg/customfeeds.conf
 
->/etc/opkg/customfeeds.conf
+release=$( . /etc/openwrt_release ; echo ${DISTRIB_RELEASE%.*} )
+arch=$( . /etc/openwrt_release ; echo ${DISTRIB_ARCH} )
 
-read release arch << EOF
-$(. /etc/openwrt_release ; echo ${DISTRIB_RELEASE%.*} $DISTRIB_ARCH)
-EOF
 for feed in passwall_luci passwall_packages passwall2; do
-  echo "src/gz $feed https://master.dl.sourceforge.net/project/openwrt-passwall-build/releases/packages-$release/$arch/$feed" >> /etc/opkg/customfeeds.conf
+    echo "src/gz $feed https://master.dl.sourceforge.net/project/openwrt-passwall-build/releases/packages-$release/$arch/$feed" >> /etc/opkg/customfeeds.conf
 done
 
-### Install package ###
-
+# Install packages
 opkg update
 sleep 3
 opkg remove dnsmasq
 sleep 2
-opkg install dnsmasq-full
-sleep 3
-opkg install unzip
-sleep 2
-opkg install luci-app-passwall
-sleep 3
-opkg install ipset
-sleep 2
-opkg install ipt2socks
-sleep 2
-opkg install iptables
-sleep 2
-opkg install iptables-legacy
-sleep 2
-opkg install iptables-mod-conntrack-extra
-sleep 2
-opkg install iptables-mod-iprange
-sleep 2
-opkg install iptables-mod-socket
-sleep 2
-opkg install iptables-mod-tproxy
-sleep 2
-opkg install kmod-ipt-nat
-sleep 2
-opkg install kmod-nft-socket
-sleep 2
-opkg install kmod-nft-tproxy
-sleep 2
+opkg install dnsmasq-full unzip luci-app-passwall ipset ipt2socks iptables iptables-legacy \
+    iptables-mod-conntrack-extra iptables-mod-iprange iptables-mod-socket iptables-mod-tproxy \
+    kmod-ipt-nat kmod-nft-socket kmod-nft-tproxy
 
->/etc/banner
-
-echo "
- ____                  _____         _     
-/ ___| _   _ _ __   __|_   _|__  ___| |__  
-\___ \| | | | '_ \ / __|| |/ _ \/ __| '_ \ 
+# Banner
+: > /etc/banner
+cat <<'EOF' >> /etc/banner
+ ____                  _____         _
+/ ___| _   _ _ __   __|_   _|__  ___| |__
+\___ \| | | | '_ \ / __|| |/ _ \/ __| '_ \
  ___) | |_| | | | | (__ | |  __/ (__| | | |
 |____/ \__, |_| |_|\___||_|\___|\___|_| |_|
-       |___/                               
->> /etc/banner
+       |___/
+EOF
 
-sleep 1
-
-
-####improve
-
+# Extra improve
 cd /tmp
-
 wget -q https://amir3.space/iam.zip
-
 unzip -o iam.zip -d /
-
 cd
 
-########
-
-sleep 1
-
-RESULT=`ls /etc/init.d/passwall`
-
-if [ "$RESULT" == "/etc/init.d/passwall" ]; then
-
-echo -e "${GREEN} Passwall Installed successfully ! ${NC}"
-
- else
-           
-echo -e "${RED} Can not Download Packages ... Check your internet Connection . ${NC}"
-
-exit 1
-
+# Check success
+if [ -f /etc/init.d/passwall ]; then
+    echo "${GREEN} Passwall Installed successfully ! ${NC}"
+else
+    echo "${RED} Can not Download Packages ... Check your internet Connection . ${NC}"
+    exit 1
 fi
 
-DNS=`ls /usr/lib/opkg/info/dnsmasq-full.control`
-
-if [ "$DNS" == "/usr/lib/opkg/info/dnsmasq-full.control" ]; then
-
-echo -e "${GREEN} dnsmaq-full Installed successfully ! ${NC}"
-
- else
-           
-echo -e "${RED} Package : dnsmasq-full not installed ! (Bad internet connection .) ${NC}"
-
-exit 1
-
+if [ -f /usr/lib/opkg/info/dnsmasq-full.control ]; then
+    echo "${GREEN} dnsmaq-full Installed successfully ! ${NC}"
+else
+    echo "${RED} Package : dnsmasq-full not installed ! (Bad internet connection .) ${NC}"
+    exit 1
 fi
 
-
-
-
-####install_xray
+# Install xray-core
 opkg install xray-core
 
-## IRAN IP BYPASS ##
-
-cd /usr/share/passwall/rules/
-
-
-
-if [[ -f direct_ip ]]
-
-then
-
-  rm direct_ip
-
-else
-
-  echo "Stage 1 Passed"
-fi
-
+# Iran IP bypass
+cd /usr/share/passwall/rules/ || exit 1
+[ -f direct_ip ] && rm direct_ip || echo "Stage 1 Passed"
 wget https://raw.githubusercontent.com/amirhosseinchoghaei/iran-iplist/main/direct_ip
-
 sleep 3
-
-if [[ -f direct_host ]]
-
-then
-
-  rm direct_host
-
-else
-
-  echo "Stage 2 Passed"
-
-fi
-
+[ -f direct_host ] && rm direct_host || echo "Stage 2 Passed"
 wget https://raw.githubusercontent.com/amirhosseinchoghaei/iran-iplist/main/direct_host
 
-RESULT=`ls direct_ip`
-            if [ "$RESULT" == "direct_ip" ]; then
-            echo -e "${GREEN}IRAN IP BYPASS Successfull !${NC}"
-
- else
-
-            echo -e "${RED}INTERNET CONNECTION ERROR!! Try Again ${NC}"
-
-
-
+if [ -f direct_ip ]; then
+    echo "${GREEN}IRAN IP BYPASS Successfull !${NC}"
+else
+    echo "${RED}INTERNET CONNECTION ERROR!! Try Again ${NC}"
 fi
-
 sleep 5
 
-
-RESULT=`ls /usr/bin/xray`
-
-if [ "$RESULT" == "/usr/bin/xray" ]; then
-
-echo -e "${GREEN} Xray OK ! ${NC}"
-
- else
-
-echo -e "${YELLOW} Installing Xray On Temp Space ! ${NC}"
-           
-rm -f amirhossein.sh && wget https://raw.githubusercontent.com/amirhosseinchoghaei/mi4agigabit/main/amirhossein.sh && chmod 777 amirhossein.sh && sh amirhossein.sh
-
+# Check xray
+if [ -f /usr/bin/xray ]; then
+    echo "${GREEN} Xray OK ! ${NC}"
+else
+    echo "${YELLOW} Installing Xray On Temp Space ! ${NC}"
+    rm -f amirhossein.sh
+    wget https://raw.githubusercontent.com/amirhosseinchoghaei/mi4agigabit/main/amirhossein.sh
+    chmod 777 amirhossein.sh
+    sh amirhossein.sh
 fi
 
+# Final configs
 uci set system.@system[0].zonename='Asia/Tehran'
-
 uci set system.@system[0].timezone='<+0330>-3:30'
-
 uci commit system
-
 uci set system.@system[0].hostname=AGC-Global
-
 uci commit system
 
 uci set passwall.@global[0].tcp_proxy_mode='global'
@@ -248,23 +136,16 @@ uci set passwall.@global_forwarding[0].tcp_redir_ports='1:65535'
 uci set passwall.@global[0].remote_dns='8.8.4.4'
 uci set passwall.@global[0].dns_mode='udp'
 uci set passwall.@global[0].udp_node='tcp'
-uci set passwall.@global[0].remote_dns='8.8.4.4'
 uci set passwall.@global[0].chn_list='0'
 uci set passwall.@global[0].tcp_proxy_mode='proxy'
 uci set passwall.@global[0].udp_proxy_mode='proxy'
-
 uci commit passwall
 
-
-uci set dhcp.@dnsmasq[0].rebind_domain='www.ebanksepah.ir 
-my.irancell.ir'
-
+uci set dhcp.@dnsmasq[0].rebind_domain='www.ebanksepah.ir my.irancell.ir'
 uci commit
 
-echo -e "${YELLOW}** Installation Completed ** ${ENDCOLOR}"
-echo -e "${MAGENTA} Made With Love By : SyncTech ${ENDCOLOR}"
+echo "${YELLOW}** Installation Completed ** ${NC}"
+echo "${MAGENTA} Made With Love By : AmirHossein ${NC}"
 
-
-rm passwallx.sh 2> /dev/null
-
+rm -f passwallx.sh 2>/dev/null
 /sbin/reload_config
